2117c4dfd391cf1564c7346caa96102871391955
ArmankoG <armanko.bg@gmail.com>
2026-07-09 02:10:36 +0300

    Добавени гетове и сетове

--- ArmanKaanPokemonBattle/Pokemon.cs
+++ ArmanKaanPokemonBattle/Pokemon.cs
@@ -7,7 +7,15 @@ using System.IO;
 
 namespace ArmanKaanPokemonBattle
 {
-    internal class Pokemon
+    public class Pokemon
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int Strength { get; set; }
+        public int Health { get; set; }
+        public string Owner { get; set; }
+        public bool IsAlive { get; set; }
     }
 }
