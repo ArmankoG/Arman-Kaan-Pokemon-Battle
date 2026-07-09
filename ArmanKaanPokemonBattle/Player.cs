b05bee97ddc26c4d852c65599f8c076799c0d1bc
ArmankoG <armanko.bg@gmail.com>
2026-07-09 11:17:16 +0300

    Инициализиран метод Player.cs, добавени гетове и сетове, направена проверка дали играча има жив покемон, чрез foreach цикъл. Оставена да се довърши функцията да се heal-ват резервните покемони

---
+++ ArmanKaanPokemonBattle/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmanKaanPokemonBattle
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+        public Pokemon ActivePokemon { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public bool HasAlivePokemon()
+        { 
+            foreach (Pokemon p in Pokemons)
+            {
+                if (p.IsAlive)
+                    return true;
+            }
+            return false;
+        }
+
+        public void HealReservePokemons()
+        {
+            //todo
+        }
+    }
+}
