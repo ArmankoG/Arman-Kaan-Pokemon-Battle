86a2b36bc0707004dae4b4f20c6bfbb7d4e5f619
chrysanthemumdog <nuzekopersonal@gmail.com>
2026-07-13 10:23:13 +0200

    Завършен Game.cs

--- ArmanKaanPokemonBattle/Game.cs
+++ ArmanKaanPokemonBattle/Game.cs
@@ -14,13 +14,36 @@ namespace ArmanKaanPokemonBattle
         public Game()
         {
             player1 = new Player("Играч 1");
-            player1 = new Player("Играч 2");
+            player2 = new Player("Играч 2");
+
+            player1.Pokemons.Add(new Pokemon(1, "Pikachu", 70, 50, 60, 100, 1));
+            player1.Pokemons.Add(new Pokemon(2, "Charizard", 85, 75, 80, 100, 1));
+            player1.Pokemons.Add(new Pokemon(3, "Blastoise", 80, 80, 80, 100, 1));
+
+            player2.Pokemons.Add(new Pokemon(4, "Mew", 60, 90, 90, 100, 2));
+            player2.Pokemons.Add(new Pokemon(5, "Gardevoir", 70, 65, 70, 100, 2));
+            player2.Pokemons.Add(new Pokemon(6, "Lucario", 80, 60, 70, 100, 2));
         }
 
-        public void StartGame() // да се довърши
+        public void StartGame()
         {
             ChoosePokemon(player1);
             ChoosePokemon(player2);
+
+            while (player1.HasAlivePokemon() && player2.HasAlivePokemon())
+            {
+                PlayerTurn(player1, player2);
+
+                if (!player2.HasAlivePokemon())
+                    break;
+
+                PlayerTurn(player2, player1);
+            }
+
+            if (player1.HasAlivePokemon())
+                Console.WriteLine("Играч 1 печели!");
+            else
+                Console.WriteLine("Играч 2 печели!");
         }
 
         private void PlayerTurn(Player current, Player enemy)
@@ -53,10 +76,28 @@ namespace ArmanKaanPokemonBattle
             current.HealReservePokemons();
         }
 
-        private void ChoosePokemon() // също така да се довърши
+        private void ChoosePokemon(Player player)
         {
             Console.WriteLine();
-            Console.WriteLine($"избери покемон:");
+            Console.WriteLine($"{player.Name} избери покемон:");
+
+            for (int i = 0; i < player.Pokemons.Count; i++)
+            {
+                if (player.Pokemons[i].IsAlive)
+                {
+                    Console.WriteLine($"{i + 1}. {player.Pokemons[i].Name} ({player.Pokemons[i].Health} HP)");
+                }
+            }
+
+            int choice = int.Parse(Console.ReadLine());
+
+            while (!player.Pokemons[choice - 1].IsAlive)
+            {
+                Console.WriteLine("Този покемон е мъртъв. Избери друг.");
+                choice = int.Parse(Console.ReadLine());
+            }
+
+            player.ActivePokemon = player.Pokemons[choice - 1];
         }
     }
 }
